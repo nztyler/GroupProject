39c86b3a5b99300aaa72e58e32d77c4049609cfd
nztyler <nztyler@hotmail.co.nz>
2015-10-05 18:11:39 +1300

    Adding more since the last commit. Added more to the classes

--- src/GameWorld/LockableDoor.java
+++ src/GameWorld/LockableDoor.java
@@ -2,4 +2,14 @@ package GameWorld;
 
 public class LockableDoor extends Door {
 
+	private boolean isLocked;
+	
+	public LockableDoor(Room room1, Room room2) {
+		super(room1, room2);
+		this.isLocked = false;
+	}
+
+	public void useKey(){
+		isLocked = !isLocked;
+	}
 }
--- src/GameWorld/objects/MovableItem.java
+++ src/GameWorld/objects/MovableItem.java
@@ -2,7 +2,6 @@ package GameWorld.objects;
 
 public class MovableItem extends Item{
 	
-	public void 
 	
 	public void drop(int index){
 		//drop the item back into the room floor
