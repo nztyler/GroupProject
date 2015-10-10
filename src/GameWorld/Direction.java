970f8a895f8d7d805c96dc0633206ce6c53a136a
nztyler <nztyler@hotmail.co.nz>
2015-10-10 21:43:06 +1300

    Added a small comment in the direction class

--- src/GameWorld/Direction.java
+++ src/GameWorld/Direction.java
@@ -7,3 +7,6 @@ package GameWorld;
  *
  */
 public enum Direction { NORTH, EAST, SOUTH, WEST}
+
+	// A direction is an enum which be used for navigating the map and
+	// movement. Also for the compass which we intend to place in the top corner
