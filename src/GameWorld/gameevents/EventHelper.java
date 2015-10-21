645c3d22f958f81fc15b7999aa0dd7af6bc751a1
GarethReeceSmith <garethreecesmith@gmail.com>
2015-10-22 03:58:09 +1300

    Initial commit of EventHelper.
    EventHelper uses Events and GameState passed to it to perform the action
    selected by the user

---
+++ src/GameWorld/gameevents/EventHelper.java
@@ -0,0 +1,14 @@
+package GameWorld.gameevents;
+
+import java.awt.event.MouseEvent;
+
+import Main.GameState;
+
+public class EventHelper {
+
+	public static void newEvent(MouseEvent event, GameState game) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
